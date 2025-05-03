beddfb7a152bc4425df630696c0ffe9e06324fba
Twouloo <tle355@outlook.com>
2025-05-03 18:34:22 +1000

    Added animation.

---
+++ observer.js
@@ -0,0 +1,36 @@
+let serviceCardEles = ['#dev-card', '#sec-card', '#data-card']
+let partnerEles = ['#partner-title', '#partner-subtitle', '#partner-imgs', '#partner-blurb']
+let securityEles = ['#security-text', '#security-img']
+let blogEles = ['#blog-text', '#blog-card-1', '#blog-card-2', '#blog-card-3']
+
+let elements = [...partnerEles, ...serviceCardEles, ...securityEles, ...blogEles].map((id) => {
+    return getElement(id);
+});
+
+createObserver();
+
+
+function getElement (id) {
+    return document.querySelector(id);
+}
+
+function createObserver() {
+    let observer;
+
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.6
+    }
+
+    observer = new IntersectionObserver(handleIntersect, options);
+    elements.forEach((element) =>{
+        observer.observe(element);
+    })
+}
+
+function handleIntersect(entires, observer)  {
+    entires.forEach((entry) => {
+       if(entry.isIntersecting) entry.target.classList.add('fade-in-soft')
+    });
+}
